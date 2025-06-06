f6e8546ad6202b3996be727ced1aebe879485ad4
Marcio Bera <marcio@manual.co>
2025-06-06 21:57:17 +0100

    initial setup with laravel/react-start-kit

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('welcome');
+})->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
 });
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
